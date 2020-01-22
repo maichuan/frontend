38754750158317c025841b1421c65f1096a907e7
dacharat <dacharat.p@ku.th>
2020-01-22 14:52:23 +0700

    :iphone: add width and height

--- src/components/restaurant/menu.jsx
+++ src/components/restaurant/menu.jsx
@@ -3,6 +3,7 @@ import { Text, Card, Left, Right, CardItem, Button } from 'native-base'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
+import { Height, Width } from '../../utils/utils'
 import MenuModal from './MenuModal'
 
 const Price = styled(Text)`
@@ -10,33 +11,60 @@ const Price = styled(Text)`
   margin: 7px 10px;
   font-weight: bold;
 `
+// const MenuButton = styled.TouchableOpacity`
+//   margin: 10px 10px 10px 0px;
+//   width: 300px;
+//   height: 150px;
+//   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+// `
+const MenuImage = styled.Image`
+  width: 150px;
+  height: 150px;
+`
+const NameView = styled.View`
+  position: absolute;
+  bottom: 0;
+  margin: 10px;
+  align-self: stretch;
+`
+const Box = styled.TouchableOpacity`
+  /* width: 100%; */
+  /* background-color: red; */
+  margin: 10px;
+  border-width: 1px;
+  border-color: blue;
+`
+const Name = styled.Text`
+  font-size: 30px;
+  font-weight: 500;
+  color: white;
+  text-shadow: 2px 2px black;
+`
 
 const Menu = ({ data }) => {
   const [showModal, setShowModal] = useState(false)
+  console.log(Height, Width)
 
   const addMenu = () => {
     setShowModal(true)
   }
 
   return (
-    <Card>
-      <CardItem>
-        <Left>
-          <Text>{data.name}</Text>
-        </Left>
-        <Right>
-          <Price>{'฿' + data.price}</Price>
-          <Button bordered onPress={() => addMenu()}>
-            <Text>Add</Text>
-          </Button>
-        </Right>
-      </CardItem>
+    <>
+      <Box onPress={() => addMenu()}>
+        {/* <MenuButton > */}
+        <MenuImage source={require('../../../assets/shrimp.jpg')} />
+        {/* </MenuButton> */}
+        <NameView>
+          <Name> {data.name}</Name>
+        </NameView>
+      </Box>
       <MenuModal
         data={data}
         showModal={showModal}
         closeModal={() => setShowModal(false)}
       />
-    </Card>
+    </>
   )
 }
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,9 @@
 import { Toast } from 'native-base'
+import { Dimensions } from 'react-native'
+
+export const Height = Dimensions.get('window').height
+
+export const Width = Dimensions.get('window').width
 
 export const displayToast = text =>
   Toast.show({
