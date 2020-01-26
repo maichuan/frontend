3c22fc52500b280a7e7f0486d4771ad4407e78fb
dacharat <dacharat.p@ku.th>
2020-01-26 16:46:49 +0700

    :iphone: update modal UI

--- src/components/restaurant/MenuModal.js
+++ src/components/restaurant/MenuModal.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Text, Button, View } from 'native-base'
 import styled from 'styled-components'
 import Modal from 'react-native-modal'
 import PropTypes from 'prop-types'
@@ -14,28 +13,84 @@ const SafeBottom = styled.SafeAreaView`
   flex: 1;
   justify-content: flex-end;
 `
-const ModalView = styled(View)`
+const ModalView = styled.View`
   background-color: #fff;
   padding: 22px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 4;
+  display: flex;
+  flex-direction: column;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
   border-color: rgba(0, 0, 0, 0.1);
-  height: 50%;
+  height: 75%;
 `
-const Quantity = styled(Text)`
-  width: 30px;
-  margin: auto 0px;
-  background-color: white;
-  text-align: center;
+const RestaurantName = styled.Text`
+  font-weight: bold;
+  font-size: 28px;
+  margin: 10px 0px;
 `
-const InlineView = styled(View)`
+const Description = styled.Text`
+  font-weight: normal;
+  font-size: 20px;
+  margin: 10px 0px;
+`
+const RowView = styled.View`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+const QuestionText = styled.Text`
+  font-weight: bold;
+  font-size: 20px;
+  margin: 10px 0px;
+`
+const QuantityView = styled.View`
+  border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+  background-color: #c4c4c4;
+  justify-content: center;
+  align-items: center;
+`
+const QuantityButton = styled.TouchableOpacity`
+  width: 40px;
+  align-items: center;
+  justify-content: center;
+`
+const QuantityAction = styled.Text`
+  font-size: 30px;
+  font-weight: 900;
+`
+const Quantity = styled.Text`
+  width: 40px;
+  background-color: white;
+  text-align: center;
+  font-size: 25px;
+`
+const ConfirmButton = styled.TouchableOpacity`
+  width: 100%;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  align-items: center;
+  justify-content: center;
+  background-color: green;
+  height: 50px;
+`
+const ComfirmText = styled.Text`
+  font-size: 25px;
+  font-weight: 500;
+  color: #fff;
+`
+const Special = styled.TextInput`
+  border-bottom-width: 1px;
+  border-bottom-color: #e5e5e5;
+  font-size: 18px;
+  margin: 10px;
 `
 
 const MenuModal = ({ data, showModal, closeModal, menusStore }) => {
   const [quantity, setQuantity] = useState(1)
+  const [special, setSpecial] = useState('')
 
   const increase = () => {
     setQuantity(quantity + 1)
@@ -45,6 +100,10 @@ const MenuModal = ({ data, showModal, closeModal, menusStore }) => {
     if (quantity > 1) {
       setQuantity(quantity - 1)
     }
+  }
+
+  const onChangeText = text => {
+    setSpecial(text)
   }
 
   const handleConfirmClicked = () => {
@@ -61,21 +120,30 @@ const MenuModal = ({ data, showModal, closeModal, menusStore }) => {
     >
       <SafeBottom>
         <ModalView>
-          <Text>I am the modal content!</Text>
-          <Text>{data.name + '(' + data.price + '.-)'}</Text>
-          <InlineView>
-            <Button bordered onPress={() => decrease()}>
-              <Text>-</Text>
-            </Button>
-            <Quantity>{quantity}</Quantity>
-            <Button bordered onPress={() => increase()}>
-              <Text>+</Text>
-            </Button>
-          </InlineView>
+          <RestaurantName>{data.name}</RestaurantName>
+          <Description>Food description Lorem ipsum</Description>
+          <RowView>
+            <QuestionText>Quantity</QuestionText>
+            <QuantityView>
+              <QuantityButton bordered onPress={() => decrease()}>
+                <QuantityAction>-</QuantityAction>
+              </QuantityButton>
+              <Quantity>{quantity}</Quantity>
+              <QuantityButton bordered onPress={() => increase()}>
+                <QuantityAction>+</QuantityAction>
+              </QuantityButton>
+            </QuantityView>
+          </RowView>
+          <QuestionText>Special Instructions</QuestionText>
+          <Special
+            onChangeText={text => onChangeText(text)}
+            placeholder="Add some special instructions here."
+            value={special}
+          />
         </ModalView>
-        <Button success onPress={() => handleConfirmClicked()}>
-          <Text>Confirm</Text>
-        </Button>
+        <ConfirmButton activeOpacity={1} onPress={() => handleConfirmClicked()}>
+          <ComfirmText>Add to Cart: {data.price * quantity} .-</ComfirmText>
+        </ConfirmButton>
       </SafeBottom>
     </BottomModal>
   )
--- src/components/restaurant/menu.jsx
+++ src/components/restaurant/menu.jsx
@@ -6,44 +6,37 @@ import PropTypes from 'prop-types'
 import { Height, Width } from '../../utils/utils'
 import MenuModal from './MenuModal'
 
-const Price = styled(Text)`
-  font-size: 20px;
-  margin: 7px 10px;
-  font-weight: bold;
-`
-// const MenuButton = styled.TouchableOpacity`
-//   margin: 10px 10px 10px 0px;
-//   width: 300px;
-//   height: 150px;
-//   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-// `
 const MenuImage = styled.Image`
-  width: 150px;
-  height: 150px;
+  width: 33%;
+  height: ${({ size }) => size};
 `
 const NameView = styled.View`
-  position: absolute;
   bottom: 0;
-  margin: 10px;
-  align-self: stretch;
+  padding: 10px;
+  width: 42%;
+`
+const PriceView = styled.View`
+  bottom: 0;
+  padding: 10px;
+  width: 25%;
+  align-items: flex-end;
 `
 const Box = styled.TouchableOpacity`
-  /* width: 100%; */
-  /* background-color: red; */
+  display: flex;
+  flex-direction: row;
   margin: 10px;
   border-width: 1px;
-  border-color: blue;
+  border-color: rgba(0, 0, 0, 0.4);
+  max-width: 500px;
+  /* box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4); */
 `
 const Name = styled.Text`
-  font-size: 30px;
+  font-size: 25px;
   font-weight: 500;
-  color: white;
-  text-shadow: 2px 2px black;
 `
 
 const Menu = ({ data }) => {
   const [showModal, setShowModal] = useState(false)
-  console.log(Height, Width)
 
   const addMenu = () => {
     setShowModal(true)
@@ -51,13 +44,19 @@ const Menu = ({ data }) => {
 
   return (
     <>
-      <Box onPress={() => addMenu()}>
+      <Box activeOpacity={0.9} onPress={() => addMenu()}>
         {/* <MenuButton > */}
-        <MenuImage source={require('../../../assets/shrimp.jpg')} />
+        <MenuImage
+          size={Width / 3}
+          source={require('../../../assets/shrimp.jpg')}
+        />
         {/* </MenuButton> */}
         <NameView>
           <Name> {data.name}</Name>
         </NameView>
+        <PriceView>
+          <Name> {data.price + '.-'}</Name>
+        </PriceView>
       </Box>
       <MenuModal
         data={data}
