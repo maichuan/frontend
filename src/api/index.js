85caf7baebdfc32e137bb6ef4a2ba9670f753c30
dacharat <dacharat.p@ku.th>
2020-04-28 15:50:32 +0700

    :basecampy: use prod API

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const serverClient = axios.create({
   // baseURL: 'http://10.2.80.247:3000',
-  baseURL: 'http://192.168.43.233:3000',
-  // baseURL: 'https://klener.df.r.appspot.com/',
+  // baseURL: 'http://192.168.43.233:3000',
+  baseURL: 'https://klener.df.r.appspot.com/',
 })
